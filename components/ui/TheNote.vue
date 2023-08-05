11cf325b03f0e6042f8aa7f69b60ee138dd447e4
vokaEntropy <60666903+vokaEntropy@users.noreply.github.com>
2023-08-05 19:42:46 +0300

    feat: update note

--- components/ui/TheNote.vue
+++ components/ui/TheNote.vue
@@ -1,13 +1,19 @@
 <template>
-  <div class="min-h-[70px] bg-teal-50 rounded-md p-5 flex flex-col gap-3 justify-start">
+  <div class="min-h-[70px] bg-teal-50 rounded-md p-5 grid gap-4">
     <div v-if="date || title">
-      <h3 v-if="title" class="text-xl font-semibold">
+      <h3 v-if="title" class="text-2xl font-semibold">
         {{ title }}
       </h3>
       <p v-if="date" class="text-sm text-gray-400">{{ date }}</p>
     </div>
 
     <p class="text-gray-600">{{ content }}</p>
+
+    <div class="self-end flex gap-1">
+      <button class="w-8 h-8 text-xl transform active:scale-95">🖊️</button>
+      <button class="w-8 h-8 text-xl transform active:scale-95 grayscale">⭐️</button>
+      <button class="w-8 h-8 text-xl transform active:scale-95">💬</button>
+    </div>
   </div>
 </template>
 
